daa87b5c9a33a03c899422321a301cea772ee1b0
mohamedabdeldayem7 <mohamed.bk10150@gmail.com>
2024-06-21 02:34:04 +0300

    initiate sakan project

---
+++ src/main/java/com/sakan/sakan/SakanApplication.java
@@ -0,0 +1,13 @@
+package com.sakan.sakan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakanApplication.class, args);
+	}
+
+}
